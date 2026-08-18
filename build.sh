51803857e9feee31c7be854736a3ee621c01ef1d
vlowingkloude <126703177+vlowingkloude@users.noreply.github.com>
2026-08-18 15:28:52 +0200

    Change container command to podman in build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export DEVUNITPASSWORD=password
 export DEV_USER=dev
 export EMAIL=nosuch@e.mail
 
-container build --build-arg DEVUNITPASSWORD=$DEVUNITPASSWORD --build-arg DEV_USER=$DEV_USER --build-arg EMAIL=$EMAIL -t devunitbase .
+podman build --build-arg DEVUNITPASSWORD=$DEVUNITPASSWORD --build-arg DEV_USER=$DEV_USER --build-arg EMAIL=$EMAIL -t devunitbase .
